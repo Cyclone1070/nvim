44d8904d2ffcd8b080e30cdf021e9023a8034c86
Hoang Mai <128883620+Cyclone1070@users.noreply.github.com>
2025-05-09 00:13:01 +1000

    remove auto extend comments

--- lua/custom/layout.lua
+++ lua/custom/layout.lua
@@ -5,3 +5,27 @@ vim.opt.expandtab = false
 -- set line wrap and break line at word end
 vim.opt.wrap = true
 vim.opt.linebreak = true
+
+local myGlobalFormatOptionsGroup = vim.api.nvim_create_augroup("MyGlobalFormatOptions", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = myGlobalFormatOptionsGroup,
+	pattern = "*",
+	callback = function(args)
+		if not args or not args.buf then
+			return
+		end
+
+		local current_fo = vim.bo[args.buf].formatoptions
+
+		if type(current_fo) ~= "string" then
+			current_fo = ""
+		end
+
+		current_fo = string.gsub(current_fo, "r", "")
+		current_fo = string.gsub(current_fo, "o", "")
+
+		vim.bo[args.buf].formatoptions = current_fo
+	end,
+	desc = "Globally disable auto-continuation of comments on new lines for all filetypes",
+})
