76e4f233af7b71ceea1379a982344a9fc8faaee6
Hoang Mai <128883620+Cyclone1070@users.noreply.github.com>
2025-05-09 15:24:06 +1000

    change <C-k> from blink to <C-r>

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -58,6 +58,10 @@ return { -- Autocompletion
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			preset = "super-tab",
 
+			-- Override <C-k> with <C-r> to enable cursor navigation in insert mode
+			["<C-k>"] = {},
+			["<C-r>"] = { "show_signature", "hide_signature", "fallback" },
+
 			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 		},
